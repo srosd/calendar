95ad3daf74f2f65fec3643ca7ccd82ac4f8674a0
Sergio <srosduran@gmail.com>
2021-01-25 19:14:49 +0100

    displaying modal and adding time and duration to events

--- src/App.js
+++ src/App.js
@@ -11,15 +11,51 @@ import Modal from './components/Modal';
 const localizer = momentLocalizer(moment);
 
 class App extends Component {
+
   state = {
-    events: [
-      {
-        start: '2021-01-01',
-        end: '2021-01-02',
-        title: "Entrega"
-      }
-    ]
+    events: [{}],
+    newEvent: {
+      start: new Date(),
+      end: new Date(),
+      time: '',
+      duration: '',
+      title: ''
+    }
   };
+
+  onChangeStart = fecha => {
+    this.setState({newEvent: {...this.state.newEvent, start: fecha}});
+  }
+  onChangeEnd = fecha => {
+    this.setState({newEvent: {...this.state.newEvent, end: fecha}})
+  }
+  onChangeTitle = e => {
+		this.setState({ newEvent: { ...this.state.newEvent, title: e.target.value } });
+  };
+  onChangeTime = e => {
+    console.log(e.target.value);
+		// this.setState({ newEvent: { ...this.state.newEvent, time: e.target.value } });
+  };
+  onChangeDuration = e => {
+    console.log(e.target.value);
+		// this.setState({ newEvent: { ...this.state.newEvent, duration: e.target.value } });
+  };
+
+  handleSubmit= e => {
+    e.preventDefault();
+    
+    // console.log(this.state.newEvent.start)
+    this.setState({ 
+      events: [...this.state.events, this.state.newEvent],
+      newEvent: {
+        start: new Date(),
+        end: new Date(),
+        title: ''
+      }
+    });
+    // myModal.hide();
+
+}
 
   render() {
     return (
@@ -32,7 +68,19 @@ class App extends Component {
           style={{ height: "100vh" }}
         />
 
-        <Modal />
+        <Modal 
+          start={this.state.newEvent.start} 
+          end={this.state.newEvent.end} 
+          onChangeStart={this.onChangeStart} 
+          onChangeEnd={this.onChangeEnd} 
+          onChangeTitle={this.onChangeTitle} 
+          handleSubmit={this.handleSubmit} 
+          newEvent={this.state.newEvent}
+          />
+
+        <button type="button" className="btn btn-warning add-event-button" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <i className="far fa-calendar-plus fa-3x"></i>
+        </button>
       </div>
 
       
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css'
 
-export const Modal = () => {
-    return (
-        <div className="modal">
-            <h1>FORMULARIO</h1>
-        </div>
-    )
+class Modal extends React.Component {
+
+    
+
+    
+    render(){
+        return (
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form className="text-center" onSubmit={this.props.handleSubmit}>
+
+                                <label htmlFor="title">Title:</label>
+                                <input 
+                                    type="text"
+                                    name="title"
+                                    value={this.props.newEvent.title}
+                                    onChange={(e)=>this.props.onChangeTitle(e)}
+                                    />
+                                <br />
+                                <label htmlFor="start">Start:</label>
+                                <DatePicker selected={this.props.start} onChange={this.props.onChangeStart}/>
+                                <br />
+                                <label htmlFor="start">End:</label>
+                                <DatePicker selected={this.props.end} onChange={this.props.onChangeEnd}/>
+                                <hr />
+                                <div className="text-end">
+                                    <button type="button" className="btn btn-danger cancel-btn" data-bs-dismiss="modal" >Cancel</button>
+                                    <button type="submit" className="btn btn-primary">Add Event</button>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
 }
 
 export default Modal;
